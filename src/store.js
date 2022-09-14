7cf7248641c9561166f3497aa3248e6eefc9d8b2
care04 <carefarrar@gmail.com>
2022-09-14 12:52:56 -0400

    Added Vuex

---
+++ src/store.js
@@ -0,0 +1,31 @@
+import { createStore } from 'vuex';
+
+const defaultTasks = [
+  { id: '1', title: 'Something', state: 'TASK_INBOX'},
+  { id: '2', title: 'Something More', state: 'TASK_INBOX'},
+  { id: '3', title: 'Something Else', state: 'TASK_INBOX'},
+  { id: '4', title: 'Something again', state: 'TASK_INBOX'},
+];
+export default createStore({
+  state: {
+    tasks: defaultTasks,
+    status: 'idle',
+    error: null,
+  },
+  mutations: {
+    ARCHIVE_TASK(state, id) {
+      state.tasks.find(task => task.id === id).state = 'TASK_ARCHIVED';
+    },
+    PIN_TASK(state, id) {
+      state.tasks.find(task => task.id === id).state = 'TASK_PINNED'
+    },
+  },
+  actions: {
+    archiveTask({ commit }, id) {
+      commit('ARCHIVE_TASK', id);
+    },
+    pinTask({ commit }, id) {
+      commit('PIN_TASK', id);
+    },
+  },
+});
